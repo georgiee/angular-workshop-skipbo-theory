67d21d98a2d4f5c9381e7f747e3ddd25da1dfe8a
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-02 14:02:36 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyDirectiveComponent } from './my-directive/my-directive.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { MySecondComponentComponent } from './my-second-component/my-second-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyDirectiveComponent,
+    MyComponentComponent,
+    MySecondComponentComponent
   ],
   imports: [
     BrowserModule
