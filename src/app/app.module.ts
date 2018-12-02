665c0ad9df4191b4692cb7a800dd8b4d76def13a
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-02 18:37:37 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SomeComponent } from './some/some.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // SomeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
