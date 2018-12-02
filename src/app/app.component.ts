73a36e96abaf4836ec1395c35a8e0319b9590fb8
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-02 17:21:54 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,38 @@
 import { Component } from '@angular/core';
 
+import { ReflectiveInjector } from '@angular/core';
+import { InjectionToken } from '@angular/core';
+
+class GreetNormal {
+  greet() {
+    console.log('Hello Sir!');
+  }
+}
+class GreetLoud {
+  greet() {
+    console.log('HELLO SIR!');
+  }
+}
+
+const GreetingService1 = new InjectionToken<string>('GreetingService');
+const GreetingService2 = new InjectionToken<string>('GreetingService');
+
+const myInjector = ReflectiveInjector.resolveAndCreate([
+  { provide: GreetingService1, useClass: GreetNormal },
+  { provide: GreetingService2, useClass: GreetLoud },
+  { provide: 'GreetingService', useClass: GreetNormal },
+  { provide: 'GreetingService', useClass: GreetLoud }
+]);
+
+const greeterA = myInjector.get(GreetingService1);
+greeterA.greet();
+
+const greeterB = myInjector.get(GreetingService2);
+greeterB.greet();
+
+const greeterC = myInjector.get('GreetingService');
+greeterC.greet();
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
