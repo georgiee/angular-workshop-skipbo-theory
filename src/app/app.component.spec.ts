bd19885ad68bf59dc01d752e94d0dbda85194e95
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-03 22:19:01 +0100

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick, flushMicrotasks } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { Component, ViewChild } from '@angular/core';
 
@@ -25,6 +25,33 @@ describe('AppComponent', () => {
     // app.elementRef.nativeElement.click();
     app.elementRef.nativeElement.dispatchEvent(new Event('click'));
     app.elementRef.nativeElement.dispatchEvent(new Event('click'));
+  }));
+
+  it('setTimeout & tick & flushMicrotasks', fakeAsync(() => {
+    let state = [];
+
+    // add to the micro task queue — will be the first thing executed after this script block is done
+    Promise.resolve().then(function() {
+      state.push('promise result');
+    });
+
+    // add to the task queue — will be executed after this task and when the micro task queue is emptied
+    setTimeout(() => { state.push('timeout called'); });
+
+    // add to the task queue after 2 seconds
+    setTimeout(() => { state.push('timeout called after 2s'); }, 2000);
+
+
+    expect(state).toEqual([]);
+
+    flushMicrotasks();
+    expect(state).toEqual(['promise result']);
+
+    tick();
+    expect(state).toEqual(['promise result', 'timeout called']);
+
+    tick(2000);
+    expect(state).toEqual(['promise result', 'timeout called', 'timeout called after 2s']);
   }));
 });
 
