9443ad0ed4d82c071db8027767b6752d28455678
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-03 18:47:21 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,6 @@ import { create } from 'rxjs-spy';
 const spy = create();
 spy.log();
 
-class Producer {
-  _counter = 0;
-  nextValue = (value) => {};
-
-  constructor(private _speed = 1000) {
-    console.log('new producer created');
-    this.next = this.next.bind(this);
-  }
-
-  next() {
-    this._counter++;
-    this.nextValue(this._counter);
-  }
-
-  start() {
-    setInterval(this.next, this._speed);
-  }
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -33,111 +15,16 @@ class Producer {
 })
 export class AppComponent {
   title = 'workshop-theory';
+  changed$ = new Subject();
 
   constructor() {
-    // this.makeCold();
-    // this.makeHot();
-    // this.makeColdHot();
-    this.makeColdHotV2();
-  }
 
-  makeColdHotV2() {
-    const myInterval2 = interval(500).pipe(
-      take(5),
-      tap(value => console.log('interval produced a value')),
-      multicast(new Subject()) // make it a ConnectableObservable
-    ) as ConnectableObservable<number>;
-
-    myInterval2.subscribe(value => console.log('received a value', value));
-    myInterval2.subscribe(value => console.log('received a value', value));
-    myInterval2.subscribe(value => console.log('received a value', value));
-
-    // let the internal subject subscribe to the given interval
-    myInterval2.connect();
-  }
-
-  makeColdHot() {
-    const myInterval = interval(500).pipe(
-      take(5),
-      tap(value => console.log('interval produced a value'))
-    );
-
-    // this will produce three intervals each delivering its own stream of numbers (1,2,3, ..)
-    // myInterval.subscribe(value => console.log('received a value', value));
-    // myInterval.subscribe(value => console.log('received a value', value));
-    // myInterval.subscribe(value => console.log('received a value', value));
-    /**
-      interval produced a value
-      received a value 0
-      interval produced a value
-      received a value 0
-      interval produced a value
-      received a value 0
-      interval produced a value
-     */
-
-     // Now look at this
-    const subject = new Subject();
-
-    // 1. let this subject susbcribe to the cold observable
-    myInterval.subscribe(subject);
-    // 2. now let future observables subscribe to the subject instead of the interval
-    subject.subscribe(value => console.log('received a value', value));
-    subject.subscribe(value => console.log('received a value', value));
-    subject.subscribe(value => console.log('received a value', value));
-
-    /**
-      interval produced a value
-      received a value 0
-      received a value 0
-      received a value 0
-     */
-  }
-
-  makeCold() {
-    console.log('make cold');
-
-    const cold = new Observable((observer) => {
-      const producer = new Producer();
-
-      producer.start();
-      producer.nextValue = value => {
-        observer.next(value);
-      };
+    this.changed$.subscribe(() => {
+      console.log('something changed')
     });
 
-    // this will create a new producer
-    setTimeout(() => {
-      cold.subscribe(value => console.log('first subscribe', value));
-    }, 1000);
-
-    // this will create another producer
-    setTimeout(() => {
-      cold.subscribe(value => console.log('second subscribe', value));
-    }, 2000);
-  }
-
-  makeHot() {
-    console.log('make hot');
-
-    const producer = new Producer(50);
-    producer.start();
-
-    const hot = new Observable((observer) => {
-      producer.nextValue = value => {
-        observer.next(value);
-      };
-    });
-
-    // some time later we subscribe
-    setTimeout(() => {
-      hot.subscribe(value => console.log('first subscribe', value));
-    }, 1000);
-
-    // this will override the callback as there is only one producer
-    setTimeout(() => {
-      hot.subscribe(value => console.log('second subscribe', value));
-    }, 2000);
-
+    this.changed$.next();
+    setTimeout(() => this.changed$.next(), 500);
+    setTimeout(() => this.changed$.next(), 1200);
   }
 }
