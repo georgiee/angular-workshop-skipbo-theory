708394869d947b803f8061d45a7606e69e4be51f
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-03 20:28:17 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { fromEvent, Observable, interval, Subject, ConnectableObservable, BehaviorSubject, range, timer } from 'rxjs';
-import { take, refCount, publish, tap, multicast, toArray, switchMap } from 'rxjs/operators';
-import { tag } from 'rxjs-spy/operators/tag';
+import { Subject } from 'rxjs';
 import { create } from 'rxjs-spy';
 
 
@@ -15,55 +13,16 @@ spy.log();
 })
 export class AppComponent {
   title = 'workshop-theory';
-  changed$ = new Subject();
+  counter = 0;
+  change$ = new Subject<boolean>();
 
   constructor() {
-    range(1, 10)
-    .pipe(
-      toArray()
-    ).subscribe(values => console.log(values));
-
-    // this.buildVariantA();
-    this.buildVariantB();
+    this.change$.subscribe(value => {
+      this.counter++;
+    });
   }
 
-  buildVariantB() {
-    timer(0, 5000)
-    .pipe(
-      tag('outer observable'),
-      // create an inner observable
-      switchMap(value => {
-        return interval(500)
-          .pipe(
-            take(2),
-            tag('inner observable'),
-            toArray()
-          );
-      }),
-      // here you will see only one value arriving from the
-      // inner observable. toArray blocks until it completes after 2 values are produced.
-      tag('after the switch'),
-    ).subscribe();
-  }
-
-  buildVariantA() {
-    timer(0, 5000)
-    .pipe(
-      tag('outer observable'),
-      // create an inner observable
-      switchMap(value => {
-        return interval(500)
-          .pipe(
-            take(2),
-            tag('inner observable')
-          );
-      }),
-      // at this point you will only receive values form the inner observable here
-      // that's how switch map works
-      // you get one value per each interval as long as the outer observable
-      // won't emit a new value — when this happens then the inner observable is unsubscribed
-      // and a new interval will be subscribed instead
-      tag('→ after the switch'),
-    ).subscribe();
+  doSomething() {
+    this.change$.next(true);
   }
 }
