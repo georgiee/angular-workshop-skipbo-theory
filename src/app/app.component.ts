acbb7da2af7c82ccb0bba69a3fc8e5f416f07d7e
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-03 08:24:46 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { tag } from 'rxjs-spy/operators/tag';
+import { create } from 'rxjs-spy';
+
+const spy = create();
+spy.log();
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'workshop-theory';
+
+  constructor() {
+    fromEvent(window, 'keydown')
+      .pipe(
+        tag('🎹 Key Pressed'),
+        take(2)
+    ).subscribe();
+
+  }
 }
