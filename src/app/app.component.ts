5db1e7fe44a445c4dd1479e207b64d0c16443fee
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-03 18:22:47 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { fromEvent, Observable, interval, Subject, ConnectableObservable } from 'rxjs';
+import { take, refCount, publish, tap, multicast } from 'rxjs/operators';
 import { tag } from 'rxjs-spy/operators/tag';
 import { create } from 'rxjs-spy';
 
@@ -35,8 +35,63 @@ export class AppComponent {
   title = 'workshop-theory';
 
   constructor() {
-    this.makeCold();
+    // this.makeCold();
     // this.makeHot();
+    // this.makeColdHot();
+    this.makeColdHotV2();
+  }
+
+  makeColdHotV2() {
+    const myInterval2 = interval(500).pipe(
+      take(5),
+      tap(value => console.log('interval produced a value')),
+      multicast(new Subject()) // make it a ConnectableObservable
+    ) as ConnectableObservable<number>;
+
+    myInterval2.subscribe(value => console.log('received a value', value));
+    myInterval2.subscribe(value => console.log('received a value', value));
+    myInterval2.subscribe(value => console.log('received a value', value));
+
+    // let the internal subject subscribe to the given interval
+    myInterval2.connect();
+  }
+
+  makeColdHot() {
+    const myInterval = interval(500).pipe(
+      take(5),
+      tap(value => console.log('interval produced a value'))
+    );
+
+    // this will produce three intervals each delivering its own stream of numbers (1,2,3, ..)
+    // myInterval.subscribe(value => console.log('received a value', value));
+    // myInterval.subscribe(value => console.log('received a value', value));
+    // myInterval.subscribe(value => console.log('received a value', value));
+    /**
+      interval produced a value
+      received a value 0
+      interval produced a value
+      received a value 0
+      interval produced a value
+      received a value 0
+      interval produced a value
+     */
+
+     // Now look at this
+    const subject = new Subject();
+
+    // 1. let this subject susbcribe to the cold observable
+    myInterval.subscribe(subject);
+    // 2. now let future observables subscribe to the subject instead of the interval
+    subject.subscribe(value => console.log('received a value', value));
+    subject.subscribe(value => console.log('received a value', value));
+    subject.subscribe(value => console.log('received a value', value));
+
+    /**
+      interval produced a value
+      received a value 0
+      received a value 0
+      received a value 0
+     */
   }
 
   makeCold() {
