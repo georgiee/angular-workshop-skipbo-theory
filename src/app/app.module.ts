8189523b5a95094b67c47c8ed7ceca4615d56735
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-03 19:07:17 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AComponent } from './a/a.component';
 
+const routes: Route[] = [
+  { path: 'a', component: AComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     AComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
